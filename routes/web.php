48330c0a87c2b518e9b7a2dfff2ece4fba96d70c
AJRMRODMAR <antoniodejesusrodriguez928@gmail.com>
2024-01-10 18:09:49 -0600

    cuarta prueba d edespliege

--- routes/web.php
+++ routes/web.php
@@ -17,65 +17,65 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/puertas', function () {
+Route::get('puertas', function () {
     return view('pages.puertas');
 });
 
-Route::get('/ventanas', function () {
+Route::get('ventanas', function () {
     return view('pages.ventanas');
 });
-Route::get('/canceles', function () {
+Route::get('canceles', function () {
     return view('pages.canceles');
 });
-Route::get('/domos', function () {
+Route::get('domos', function () {
     return view('pages.domos');
 });
-Route::get('/vitrinas', function () {
+Route::get('vitrinas', function () {
     return view('pages.vitrinas');
 });
-Route::get('/barandales', function () {
+Route::get('barandales', function () {
     return view('pages.barandales');
 });
 
 
 
-Route::get('/vidrios', function () {
+Route::get('vidrios', function () {
     return view('pages.vidrios');
 });
-Route::get('/policarbonatos', function () {
+Route::get('policarbonatos', function () {
     return view('pages.policarbonatos');
 });
-Route::get('/aluminios', function () {
+Route::get('aluminios', function () {
     return view('pages.aluminios');
 });
-Route::get('/herrajes', function () {
+Route::get('herrajes', function () {
     return view('pages.herrajes');
 });
 
 
-Route::get('/esquineros', function () {
+Route::get('esquineros', function () {
     return view('pages.esquineros');
 });
-Route::get('/jardin', function () {
+Route::get('jardin', function () {
     return view('pages.jardin');
 });
-Route::get('/hurnas', function () {
+Route::get('hurnas', function () {
     return view('pages.hurnas');
 });
-Route::get('/peceras', function () {
+Route::get('peceras', function () {
     return view('pages.peceras');
 });
-Route::get('/arreglos', function () {
+Route::get('arreglos', function () {
     return view('pages.arreglos');
 });
 
-Route::get('/error404', function () {
-    return view('error.error404'); // El nombre de la vista corresponde al archivo HTML personalizado
+Route::get('error404', function () {
+    return view('error.error404');  //El nombre de la vista corresponde al archivo HTML personalizado
 });
 
 
 
 
-Route::get('/presupuesto', function () {
+Route::get('presupuesto', function () {
     return view('pages.presupuesto');
 });
